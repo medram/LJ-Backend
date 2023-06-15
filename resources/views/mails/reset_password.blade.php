11c16cf1619a9c5c685f27a652e4e1a79b10c6ca
medram <mramouchy@gmail.com>
2023-06-15 13:00:04 +0100

    reset_password email template updated

--- resources/views/mails/reset_password.blade.php
+++ resources/views/mails/reset_password.blade.php
@@ -1,5 +1,6 @@
 Hello {{ $USERNAME }}<br>
 Please click the link bellow to reset your password:<br>
 <a href="{{ $RESET_PASSWORD_LINK }}">{{ $RESET_PASSWORD_LINK }}</a>
-
+<br>
+<br>
 Have a great day.
