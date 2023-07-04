106da734e234b05bc4b47439f62943ff3b754a24
medram <mramouchy@gmail.com>
2023-07-04 22:49:43 +0100

    Migrations updated

---
+++ database/migrations/2023_07_04_132434_upldate_chats.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('chats', function (Blueprint $table) {
+            $table->renameColumn('name', 'title');
+            $table->renameColumn('hash', 'uuid');
+            $table->string('path')->nullable()->change();
+
+            $table->longText("chat_history");
+
+            $table->unique("uuid");
+            $table->index("uuid");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        $table->dropUnique("uuid");
+        $table->dropIndex("uuid");
+
+        $table->dropColumn("chat_history");
+
+        $table->renameColumn('title', 'name');
+        $table->renameColumn('uuid', 'hash');
+    }
+};
