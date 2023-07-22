8664f5bc15d52217152c84f324723234dbd1efe1
medram <mramouchy@gmail.com>
2023-07-22 23:36:19 +0100

    Installer Routes added

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,13 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\InstallController;
+use App\Http\Controllers\FrontendController;
 
 use App\Http\Middleware\UserRequired;
 use App\Http\Middleware\AdminRequired;
+
+use App\Http\Middleware\InstallerMiddleware;
 
 
 /*
@@ -20,12 +24,29 @@ use App\Http\Middleware\AdminRequired;
 |
 */
 
-
 # For subscription activation (after payment).
 Route::get('/checkout/validate/subscription/{id}/{user_id}', [CheckoutController::class, "validateSubscription"])->name("checkout.validate");
 
 
 # For user account verification.
 Route::get('/verify/{token}', [UserController::class, "verifyAccount"])
-        ->where("token", "[a-zA-Z0-9]+")
-        ->name('verify_account');
+    ->where("token", "[a-zA-Z0-9]+")
+    ->name('verify_account');
+
+
+# Installer
+Route::group(["prefix" => "/install", "middleware" => InstallerMiddleware::class], function () {
+    Route::controller(InstallController::class)->group(function (){
+
+        Route::get("/", "index")->name("install.index");
+        Route::get("/requirements", "requirements")->name("install.requirements");
+        Route::get("/database", "database")->name("install.database");
+        Route::post("/database", "database")->name("install.database.post");
+        Route::get("/install", "installDatabase")->name("install.database.install");
+        Route::get("/completed", "completed")->name("install.completed");
+
+    });
+});
+
+# Home page (For Frontent React Project)
+Route::fallback([FrontendController::class, "index"])->name("frontend")->middleware(InstallerMiddleware::class);
