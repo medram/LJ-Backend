2679f8e67e273c13ea1138d32c9ea1bd793c2f31
medram <mramouchy@gmail.com>
2024-02-08 22:52:06 +0100

    Stripe webhook events registered

--- config/payment_gateways.php
+++ config/payment_gateways.php
@@ -3,9 +3,7 @@
 return [
 
 	"paypal" => [
-
 		"WEBHOOK_URL" => isset($_SERVER['HTTP_HOST']) ? "https://{$_SERVER['HTTP_HOST']}/api/v1/webhook/paypal" : "",
-
 		"WEBHOOK_EVENTS" => [
 		    "PAYMENT.SALE.COMPLETED",
 		    "BILLING.SUBSCRIPTION.ACTIVATED",
@@ -16,4 +14,14 @@ return [
 		],
 	],
 
+	"stripe" => [
+		"WEBHOOK_URL" => isset($_SERVER['HTTP_HOST']) ? "https://{$_SERVER['HTTP_HOST']}/api/v1/webhook/stripe" : "",
+		"WEBHOOK_EVENTS" => [
+			"invoice.paid",
+			"customer.subscription.updated",
+			"customer.subscription.deleted",
+			"customer.subscription.trial_will_end",
+			"invoice.payment_failed",
+		]
+	]
 ];
