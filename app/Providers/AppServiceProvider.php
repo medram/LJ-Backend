09680e17c2e9e16f27bb10d345cefa428a9c8670
medram <mramouchy@gmail.com>
2024-06-27 21:41:48 +0100

    Fixed: DB index length for Laravel migration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // Fix the db index length for migrations
+        Schema::defaultStringLength(191);
+        
         if($this->app->environment('production')) {
             \URL::forceScheme('https');
         }
